b3306bfd4dda4c085a2125954504f3bb1ff6392d
Max <ozaka@mail.ru>
2019-04-01 17:26:19 +0300

    сделал задание 1 и 4

---
+++ js/3_urok.js
@@ -0,0 +1,33 @@
+// задание №1
+butdz3n1.onclick = function () {
+    var n = 1;
+    while (n < 100) {
+        n++;
+        var x = 1;
+        while (x < n) {
+            x++;
+            if (n % x == 0) break;
+        }
+        if (x == n) console.log(n);
+    }
+}
+
+// задание №2
+butdz3n2.onclick = function () {
+
+}
+
+// задание №3
+butdz3n3.onclick = function () {
+
+}
+
+// задание №4
+butdz3n4.onclick = function () {
+    for (var i = 0; i < 10; console.log(i++)) {}
+}
+
+// задание №5
+butdz3n5.onclick = function () {
+
+}
