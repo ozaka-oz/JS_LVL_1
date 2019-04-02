fa8a5c2a9d7329ea3846dec2f071f161c988bbc0
Max <ozaka@mail.ru>
2019-04-02 16:14:24 +0300

    Закончил дз 3

--- js/3_urok.js
+++ js/3_urok.js
@@ -14,12 +14,32 @@ butdz3n1.onclick = function () {
 
 // задание №2
 butdz3n2.onclick = function () {
-
+    let arrPrices = [2200, 5550, 3000, 1800];
+    function basketSum (goods) {
+        let sum = 0;
+        for (let i = 0; i < arrPrices.length; i++) {
+            sum += goods[i]
+        }
+        console.log(sum);
+    }
+    basketSum(arrPrices);
 }
 
 // задание №3
 butdz3n3.onclick = function () {
-
+    let arrGoods = ["Щит Капитана Америки", "Перчатка Таноса", "Вибраниум 1 кг", "Мьольнир"];
+    let arrPrices = [3200, 5550, 3000, 1800];
+        function countBasketPrice (goods) {
+        for (i = 0; i < arrGoods.length; i++){
+            console.log (arrGoods[i] + " - " + arrPrices[i])
+        }
+        let sum = 0;
+        for (let i = 0; i < arrPrices.length; i++) {
+            sum += goods[i]
+        }
+        console.log("Общая стоимость: " + sum);
+    }
+    countBasketPrice(arrPrices);
 }
 
 // задание №4
